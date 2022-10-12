114a1190657bcdd7c499b84e9025571a8609f7fe
Victor Samoylik <v.samojlik@gmail.com>
2022-10-12 23:01:51 +0300

    feat: solve Stack

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.storage = [];
+  }
+  push(element) {
+    return this.storage.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.storage.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.storage[this.storage.length - 1];
   }
 }
 
